09740e02c13a3c7919044a36c5b1ffd35a2fb406
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2026-05-26 18:54:44 +0200

    ft: progress for backend with server.ts and config

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from "express";
+import type { Request, Response } from "express";
+import cors from "cors";
+
+const app = express();
+const port = 3000;
+app.use(cors());
+
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
