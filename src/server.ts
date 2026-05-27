c70b8ef0c5d91f1f08001878f1929d72cdacc067
unknown <kristoffer.larsson.02@gmail.com>
2026-05-27 14:40:05 +0200

    feat: connection of mongodb and integration of .env and varaibles

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
+
+dotenv.config();
 
 const app = express();
-const port = 3000;
-app.use(cors());
 
+connectDB();
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
