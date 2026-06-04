5bfd8d99816c7cf047f46495cd0fad3ddab23e37
Kristoffer-L <kristoffer.larsson.02@gmail.com>
2026-06-04 16:39:53 +0200

    test: origins for express cors

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,17 @@ const io = new Server(httpServer, {
 });
 
 connectDB();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://examensarbete-frontend.vercel.app",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/users", userRoutes);
